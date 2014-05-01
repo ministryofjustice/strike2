b7cdcf65ab8e8f6e055ac1bfb6d7cf845df252fd
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-05-01 17:01:56 +0100

    Version bump up to 0.3.0
    Updated the README and sample upstart script

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject strike2 "0.2.1"
+(defproject strike2 "0.3.0"
   :description "App for doing strikes on PDFs"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
