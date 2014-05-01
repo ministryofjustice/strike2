eca66a8b6b43e7bc06ae34897c168183e415c005
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-05-01 17:34:07 +0100

    Added tests

---
+++ test/strike2/test/cross_out.clj
@@ -0,0 +1,11 @@
+(ns strike2.test.cross-out
+  (:use clojure.test
+        strike2.cross-out))
+
+(deftest stiking-functionality
+  (testing "strike-out"
+    (let [data {:input "/Users/alex/dev/cc/strike2/form.pdf" ;; TODO: fix this sometime
+                :output "/tmp/blah.pdf"
+                :strikes [{:page 2, :x 267, :y 557, :x1 40, :y1 0, :thickness 2}
+                          {:page 2, :x 309, :y 557, :x1 37, :y1 0, :thickness 2}]}]
+    (is (= (strike-out data) "/tmp/blah.pdf")))))
--- test/strike2/test/handler.clj
+++ test/strike2/test/handler.clj
@@ -1,14 +1,18 @@
 (ns strike2.test.handler
   (:use clojure.test
-        ring.mock.request  
+        ring.mock.request
         strike2.handler))
 
 (deftest test-app
   (testing "main route"
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-  
+      (is (= (:body response) "The app is up"))))
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (is (= (:status response) 404))))
+
+  (testing "/health"
+    (let [response (app (request :get "/health"))]
+      (is (= (:status response) 200)))))
