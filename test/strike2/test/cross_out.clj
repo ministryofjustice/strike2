a2cb5f1c92945b0ee9220c6fa6cbac88711acfa4
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2014-07-17 11:29:17 +0100

    Converting the relative file path to full path
    I'm doing this as the input has to be the full path to file, as a
    string.

--- test/strike2/test/cross_out.clj
+++ test/strike2/test/cross_out.clj
@@ -6,7 +6,7 @@
 (deftest striking-functionality
   (testing "strike-out"
     (let [pdf-output "/tmp/blah.pdf"
-          data {:input (io/resource "pdf/form.pdf")
+          data {:input (str (io/file (io/resource "pdf/form.pdf")))
                 :output pdf-output
                 :strikes [{:page 2, :x 267, :y 557, :x1 40, :y1 0, :thickness 2}
                           {:page 2, :x 309, :y 557, :x1 37, :y1 0, :thickness 2}]}]
