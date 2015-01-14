711d8c8f4350807af76f484cf02f2be5196127a3
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-01-14 15:08:03 +0000

    Created a test script to test a hypothesis
    We are getting a certain exception in production where the error is
    complaining about 'too many open files'.
    
    This script is there to help us reproduce the error in staging in order
    to fix it.

---
+++ scripts/test-with-different-input.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# for testing multiple PDFs sent accross to the service
+
+# copy pdf file to file
+file="/tmp/striky-$(date +%s)${RANDOM}"
+
+cp ../form.pdf ${file}.pdf
+
+# write the below JSON to a file
+cat <<EOF > ${file}.json
+{
+    "input": "${file}.pdf",
+    "output": "/tmp/blah.pdf",
+    "strikes": [
+        { "page": 2, "x": 267, "y": 557, "x1": 40, "y1": 0, "thickness": 2 },
+        { "page": 2, "x": 309, "y": 557, "x1": 37, "y1": 0, "thickness": 2 }
+    ],
+    "flatten": true
+}
+EOF
+
+# post the request
+curl -H "Accept: application/json" \
+    -X POST \
+    -H "Content-Type: application/json" \
+    -d @scripts/${file}.json http://127.0.0.1:4000/
