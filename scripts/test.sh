12420423bdaf868c6aa8c2121988eac8d1622131
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-01-14 15:06:08 +0000

    Changed test script host to 127.0.0.1
    Because the app is explicitly serving from 127.0.0.1 instead of
    localhost, which implies all the interfaces. Which is not what we want
    in production.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 # test the app manually
 
 curl -H "Accept: application/json" \
     -X POST \
     -H "Content-Type: application/json" \
-    -d @scripts/strikes.json http://localhost:4000/
+    -d @scripts/strikes.json http://127.0.0.1:4000/
