938eb2bb6d06cd87c13a16a6460a293ffd0d9a48
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-01-26 11:41:57 +0000

    Bumped up version to 0.6.2
    Required for successful deployment in prod. An issue occurred where we
    had 0.6.0 in project.clj and the tag was 0.6.1. This should fix it.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject strike2 "0.6.0"
+(defproject strike2 "0.6.2"
   :description "App for doing strikes on PDFs"
   :url "https://github.com/ministryofjustice/strike2"
   :dependencies [[org.clojure/clojure "1.6.0"]
